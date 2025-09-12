386c105aaf195513d8e24a09dc50aaa60100c864
angc <angcoder.c@gmail.com>
2025-09-11 18:57:44 -0600

    entorno de wsl configurado

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,26 +3,37 @@
 set -e
 echo "PROYECTO 1-PACMAN-SETUP"
 
-sudo apt install python3-pip
-sudo apt install python3.12-venv
+sudo apt install -y python3-pip
+sudo apt install -y python3.12-venv
+
+# virtual env
 python3 -m venv venv
 source ./venv/bin/activate
+mkdir -p build
+mkdir -p bin
 
+# Instalar Conan si no está disponible
 if ! command -v conan &> /dev/null; then
     echo "INSTALANDO CONAN"
-    pip install --user conan
+    pip install conan
 fi
+
+# dev profile for conan
+if ! conan profile list | grep -q "^dev$"; then
+    echo "CREANDO PROFILE dev..."
+    conan profile detect --name dev
+fi
+
 echo "INSTALANDO CMAKE Y NCURSES..."
 sudo apt install -y cmake libncurses5-dev libncursesw5-dev
 
 echo "INSTALANDO DEPENDENCIAS..."
-conan install . --output-folder=build --build=missing -s build_type=Release
+conan install . --output-folder=build --build=missing -s build_type=Release --profile:build=dev
 
 echo "CONFIGURACION CON CMAKE"
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin
 
 echo "COMPILACION..."
-mkdir bin
-cmake --build bin
+cmake --build build
 
 echo "LISTO"
